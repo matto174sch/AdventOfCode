d13d1cc74faf99f23effeb97e0efab2e55b1246b
matto174 <matto174@student.liu.se>
2022-12-03 16:00:20 +0100

    day 3 task 2 solved

--- main.py
+++ main.py
@@ -9,17 +9,19 @@ def main():
 
     data = open("solutions/resources/day2.txt", "r")
     data2 = open("solutions/resources/day2.txt", "r")
-    print("Day 2:")
+    print("\nDay 2:")
     print(f"> Solution Task 1: {day2.task1(data)}")
     print(f"> Solution Task 2: {day2.task2(data2)}")
     data.close()
     data2.close()
 
-    task1_3 = open("solutions/resources/day3.txt", "r")
-    print("Day 3:")
-    print(f"> Solution Task 1: {day3.task1(task1_3)}")
-    #print(f"> Solution Task 2: {day2.task2(data2)}")
-    task1_3.close()
+    task13 = open("solutions/resources/day3.txt", "r")
+    print("\nDay 3:")
+    print(f"> Solution Task 1: {day3.task1(task13)}")
+    task13.close()
+    task23 = open("solutions/resources/day3.txt", "r")
+    print(f"> Solution Task 2: {day3.task2(task23)}")
+    task23.close()
 
 
     return None
--- solutions/day3.py
+++ solutions/day3.py
@@ -17,6 +17,9 @@ def task1(data):
                 shares.append(item)
                 seen.append(item)
 
+    return get_sum_prios(shares)
+
+def get_sum_prios(shares):
     letters = list(string.ascii_letters)
     sum_prio = 0
     for share in shares:
@@ -24,8 +27,36 @@ def task1(data):
 
     return sum_prio
 
+def task2(data):
+    i = 0
+    j = 1 
+    groups = {}
+    group = list()
+    for rucksack in data:
+        group.append(rucksack.rstrip())
+        i += 1
+        if i == 3:
+            groups[j] = group
+            group = list()
+            i = 0
+            j += 1
+
+    shares = list()
+    for group in groups.values():
+        for bag in group:
+            seen = list()
+            for item in bag:
+                if item in group[1] and item in group[2] and item not in seen:
+                    seen.append(item)
+                    shares.append(item)
+            break
+
+    return get_sum_prios(shares)
+
+
 def main():
     print(task1(DATA))
+    print(task2(DATA))
     DATA.close()
     return
 
